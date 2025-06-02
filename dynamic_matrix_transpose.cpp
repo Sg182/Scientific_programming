dd1fe689733322a0cb0eea2bc62ecea5c9f1ed4f
sg182 <swarnamoyghosh@Swarnamoys-MacBook-Pro.local>
2025-06-02 15:52:30 -0500

    debugged and run

--- dynamic_matrix_transpose.cpp
+++ dynamic_matrix_transpose.cpp
@@ -8,4 +8,43 @@ int main(){
     cout << "Enter the number of rows and columns respectively: "<<endl;
     cin >>  rows >> cols;
     cout << "You entered rows: "<<rows<<" columns: "<< cols<<endl;
+
+    // now initialize a 'matrix' to allocate memory (pointer to pointer)
+    int** matrix = (int**)malloc(rows*sizeof(int*));
+
+    for (int i=0; i<rows; ++i){
+        matrix[i]= (int*)malloc(cols*sizeof(int)); // now initiate memory (pointers to integers) for each rows
+    }
+
+    for (int i=0; i<rows; ++i){
+        for (int j =0; j<cols;++j){
+            cout << "Enter the elements of row "<<i+1<<endl;
+            cin >> matrix[i][j];}
+    }
+
+    // print the original matrix
+    cout <<"The original matrix"<<endl;
+    for (int i=0; i<rows; ++i){
+        for (int j =0; j<cols;++j){
+         
+        cout << matrix[i][j]<< " ";}
+        cout <<endl;
+    }
+    cout <<"---------------------"<<endl;
+    cout <<"The transposed matrix is "<<endl;
+
+    for (int j=0; j<cols; ++j){
+        for (int i =0; i<rows;++i){
+         
+        cout << matrix[i][j]<< " ";}
+        cout <<endl;
+    }
+
+    // free the memory
+
+    for (int i=0; i<rows; ++i){
+        free(matrix[i]);
+    }
+    free(matrix);
+
 }
