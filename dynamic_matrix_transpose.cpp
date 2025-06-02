a9def6bc25b121a52d668a4be7e3dc06d0951c52
sg182 <swarnamoyghosh@Swarnamoys-MacBook-Pro.local>
2025-06-01 22:50:46 -0500

    first commit

---
+++ dynamic_matrix_transpose.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    int cols,rows;
+    
+    cout << "Enter the number of rows and columns respectively: "<<endl;
+    cin >>  rows >> cols;
+    cout << "You entered rows: "<<rows<<" columns: "<< cols<<endl;
+}
